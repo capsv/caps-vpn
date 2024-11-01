445a8c81b1cb76e269f99a3ac4baddea576c89e1
trapper <trapp.umin@gmail.com>
2024-11-01 19:22:18 +0300

    feat(user-management-service): create service

--- user-management-service/src/main/java/org/capsule/com/vpn/UserManagementServiceApplication.java
+++ user-management-service/src/main/java/org/capsule/com/vpn/UserManagementServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CapsuleVPNApplication {
+public class UserManagementServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CapsuleVPNApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementServiceApplication.class, args);
+    }
 
 }
